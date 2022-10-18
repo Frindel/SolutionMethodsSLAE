9fd004d1247d6b1875a427dcafdea9515dd2e57a
Frindel <m_storogev@mail.ru>
2022-10-18 15:40:56 +0300

    Добавьте файлы проекта.

---
+++ SolutionMethodsSLAE/ViewModel/MainWindowVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionMethodsSLAE.ViewModel
+{
+	internal class MainWindowVM
+	{
+		#region Events
+
+		#endregion
+
+		#region Propertys
+
+		#endregion
+
+		#region Commands
+
+		#endregion
+
+		#region Constructors
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+	}
+}
